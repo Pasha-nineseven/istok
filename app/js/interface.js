09549ca00bf1fa6ad1897c29508f215d5c54ac2c
Pasha-nineseven <pavel@nineseven.ru>
2017-12-10 23:36:45 +0300

    up

--- app/js/interface.js
+++ app/js/interface.js
@@ -259,6 +259,15 @@ $(document).ready(function() {
 
 		//midClick: true,
 	});
+
+
+	//SEARCH FOCUS
+	$('body').on('focus','.search-form__input', function(){
+		$(this).parents('.search-form').addClass('search-form--focus');
+	});
+	$('body').on('focusout','.search-form__input', function(){
+		$(this).parents('.search-form').removeClass('search-form--focus');
+	});
 });
 
 
@@ -280,12 +289,16 @@ $('body').append(
 	'<div style="position: fixed; z-index: 1005; bottom: 0; right: 0; background: #fff; border: solid 1px #828286; width: 200px;"> \
 		<a href="javascript:void(0);" style="float: right;background:#ccc; color:#000; padding: 5px 10px; text-decoration: none; font-size: 16px" onclick="$(this).parent().hide()">Close X</a> \
 	<style> \
-		#pages { padding: 10px 20px 0 50px; font-size: 18px; } \
+		#pages { padding: 10px 20px 0 50px; font-size: 14px; } \
 		#pages a { text-decoration: none; } \
 		#pages li { margin: 5px 0; } \
 	</style> \
 	<ol id="pages"> \
-		<li><a href="about.html">About</a></li> \
-		<li><a href="index.html">Index</a></li> \
+		<li><a href="about.html">О нас</a></li> \
+		<li><a href="index.html">Главная</a></li> \
+		<li><a href="news.html">Новости</a></li> \
+		<li><a href="news-in.html">Новости-внутр</a></li> \
+		<li><a href="search.html">Поиск</a></li> \
+		<li><a href="catalog.html">Каталог</a></li> \
 	</ol> \
 </div>');
